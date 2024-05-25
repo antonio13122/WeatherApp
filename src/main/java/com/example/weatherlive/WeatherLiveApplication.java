56b98d6864376f0f26257193e834897e866deb67
Antonio <tonisajbic501@gmail.com>
2024-05-25 21:31:55 +0200

    College project - weather app in Java

---
+++ src/main/java/com/example/weatherlive/WeatherLiveApplication.java
@@ -0,0 +1,12 @@
+package com.example.weatherlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherLiveApplication.class, args);
+	}
+}
